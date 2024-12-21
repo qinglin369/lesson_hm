5fb94b9118171ed16d2435ac7170fa7ad88e9e19
qinglinxiao <1990288059@qq.come>
2024-12-21 19:24:29 +0800

    react-vue初体验

--- JS/yayu/type2/9.js
+++ JS/yayu/type2/9.js
@@ -1,3 +1,5 @@
 console.log(null+1)
 console.log({} + []);
 console.log([] == ![])
+console.log(parseInt("0xFF"))
+console.log(parseFloat(".1")) // 0.1
---
+++ vue_react/cli/try-vue/src/components/Counter.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="counter">
+        <!-- focus 业务的 数据驱动思想 -->
+    <p>Count:{{ count }}</p>
+    <button @click="increment">+1</button>
+        计数
+    </div>
+</template>
+<script setup>
+import{
+    ref
+} from 'vue'
+const count = ref(0)
+
+const increment = () => {
+    count.value++
+}
+</script>
+<style>
+.counter{
+    text-align: center;
+    margin-top: 50px;
+}
+.button{
+   padding: 10px,20px;
+   font-size: 15px;
+}
+</style>
